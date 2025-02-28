eda231ec4f70fee0ab4e11e56840bd9fcc9228a2
clmntgbr <clement.goubier@gmail.com>
2025-02-28 13:48:40 +0100

    Add Makefile for code formatting and linting; update requirements for new tools

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from src.s3_client import S3Client
 from src.rabbitmq_client import RabbitMQClient
 from src.file_client import FileClient
 from src.converter import ProtobufConverter
-from src.Protobuf.Message_pb2 import ApiToSubtitleIncrustator, MediaPodStatus, Video, MediaPod
+from src.Protobuf.Message_pb2 import ApiToSubtitleIncrustator, MediaPodStatus, MediaPod
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,24 +17,26 @@ s3_client = S3Client(Config)
 rmq_client = RabbitMQClient()
 file_client = FileClient()
 
-celery = Celery(
-    'tasks',
-    broker=app.config['RABBITMQ_URL']
+celery = Celery("tasks", broker=app.config["RABBITMQ_URL"])
+
+celery.conf.update(
+    {
+        "task_serializer": "json",
+        "accept_content": ["json"],
+        "broker_connection_retry_on_startup": True,
+        "task_routes": {
+            "tasks.process_message": {"queue": app.config["RMQ_QUEUE_WRITE"]}
+        },
+        "task_queues": [
+            Queue(
+                app.config["RMQ_QUEUE_READ"], routing_key=app.config["RMQ_QUEUE_READ"]
+            )
+        ],
+    }
 )
 
-celery.conf.update({
-    'task_serializer': 'json',
-    'accept_content': ['json'],
-    'broker_connection_retry_on_startup': True,
-    'task_routes': {
-        'tasks.process_message': {'queue': app.config['RMQ_QUEUE_WRITE']}
-    },
-    'task_queues': [
-        Queue(app.config['RMQ_QUEUE_READ'], routing_key=app.config['RMQ_QUEUE_READ'])
-    ],
-})
 
-@celery.task(name='tasks.process_message', queue=app.config['RMQ_QUEUE_READ'])
+@celery.task(name="tasks.process_message", queue=app.config["RMQ_QUEUE_READ"])
 def process_message(message):
     mediaPod: MediaPod = ProtobufConverter.json_to_protobuf(message)
     protobuf = ApiToSubtitleIncrustator()
@@ -58,24 +60,32 @@ def process_message(message):
 
         if not s3_client.download_file(keyVideo, tmpVideoFilePath):
             raise Exception
-        
+
         ffmpeg.input(tmpVideoFilePath).output(
-            tmpProcessedVideoFilePath, 
+            tmpProcessedVideoFilePath,
             vf=f"subtitles={tmpAssFilePath}",
-            vcodec="libx264", crf=23, preset="fast",
+            vcodec="libx264",
+            crf=23,
+            preset="fast",
         ).run()
 
         if not s3_client.upload_file(tmpProcessedVideoFilePath, keyVideoProcessed):
             raise Exception
-        
-        protobuf.mediaPod.status = MediaPodStatus.Name(MediaPodStatus.SUBTITLE_INCRUSTATOR_COMPLETE)
+
+        protobuf.mediaPod.status = MediaPodStatus.Name(
+            MediaPodStatus.SUBTITLE_INCRUSTATOR_COMPLETE
+        )
 
         file_client.delete_file(tmpAssFilePath)
         file_client.delete_file(tmpProcessedVideoFilePath)
         file_client.delete_file(tmpVideoFilePath)
 
         rmq_client.send_message(protobuf, "App\\Protobuf\\SubtitleIncrustatorToApi")
-    except Exception as e:
-        protobuf.mediaPod.status = MediaPodStatus.Name(MediaPodStatus.SUBTITLE_INCRUSTATOR_ERROR)
-        if not rmq_client.send_message(protobuf, "App\\Protobuf\\SubtitleIncrustatorToApi"):
+    except Exception:
+        protobuf.mediaPod.status = MediaPodStatus.Name(
+            MediaPodStatus.SUBTITLE_INCRUSTATOR_ERROR
+        )
+        if not rmq_client.send_message(
+            protobuf, "App\\Protobuf\\SubtitleIncrustatorToApi"
+        ):
             return False
